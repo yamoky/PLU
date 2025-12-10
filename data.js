c56f645e321a701ccaa4b9a47c3d3eefed7f652e
yamoky <cyrille.closier@gmail.com>
2025-12-10 22:48:18 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,49 +1,195 @@
 // data.js
-window.PLU_DATA = (function(){
+// Base de données des produits PLU + images
+
+window.PLU_DATA = (function () {
   const products = [
-    { code: "1",  name: "Banane",         nom: "Banane",         image: "banane.jpg" },
-    { code: "2",  name: "Tomate",         nom: "Tomate",         image: "tomate_grappe.jpg" },
-    { code: "3",  name: "Concombre",      nom: "Concombre",      image: "concombre.jpg" },
-    { code: "4",  name: "Courgette",      nom: "Courgette",      image: "courgette_vrac.jpg" },
-    { code: "5",  name: "Melon",          nom: "Melon",          image: "melon.jpg" },
-    { code: "6",  name: "Avocat",         nom: "Avocat",         image: "avocat_vrac.jpg" },
-    { code: "7",  name: "Batavia",        nom: "Batavia",        image: "batavia.jpg" },
-    { code: "8",  name: "Kiwi Vert",      nom: "Kiwi Vert",      image: "kiwi_vrac.jpg" },
-    { code: "9",  name: "Radis Botte",    nom: "Radis Botte",    image: "radis_vrac.jpg" },
-    { code: "10", name: "Raisin Blanc",   nom: "Raisin Blanc",   image: "raisin_blanc.jpg" },
-    { code: "11", name: "Aubergine",      nom: "Aubergine",      image: "aubergine_vrac.jpg" },
-    { code: "12", name: "Chou-fleur",     nom: "Chou-fleur",     image: "chou_fleur.jpg" },
-    { code: "13", name: "Poire Verte",    nom: "Poire Verte",    image: "poire_vrac.jpg" },
-    { code: "14", name: "Mangue",         nom: "Mangue",         image: "mangue_vrac.jpg" },
-    { code: "15", name: "Pomelo",         nom: "Pomelo",         image: "pamplemousse_vrac.jpg" },
-    { code: "16", name: "Ananas",         nom: "Ananas",         image: "ananas.jpg" },
-    { code: "17", name: "Nectarine",      nom: "Nectarine",      image: "nectarine.jpg" },
-    { code: "18", name: "Pomme Bicolore", nom: "Pomme Bicolore", image: "pomme_gala.jpg" },
-    { code: "19", name: "Pomme Golden",   nom: "Pomme Golden",   image: "pomme_golden.jpg" },
-    { code: "20", name: "Patate Douce",   nom: "Patate Douce",   image: "patate_douce_vrac.jpg" },
+    // Fruits & légumes
+    {
+      code: "1",
+      name: "Banane",
+      nom: "Banane",
+      image: "assets/banane.jpg",
+    },
+    {
+      code: "2",
+      name: "Grappe de tomate",
+      nom: "Grappe de tomate",
+      image: "assets/tomate_grappe.jpg",
+    },
+    {
+      code: "3",
+      name: "Concombre",
+      nom: "Concombre",
+      image: "assets/concombre.jpg",
+    },
+    {
+      code: "4",
+      name: "Courgette",
+      nom: "Courgette",
+      image: "assets/courgette_vrac.jpg",
+    },
+    {
+      code: "5",
+      name: "Melon",
+      nom: "Melon",
+      image: "assets/melon.jpg",
+    },
+    {
+      code: "6",
+      name: "Avocat",
+      nom: "Avocat",
+      image: "assets/avocat_vrac.jpg",
+    },
+    {
+      code: "7",
+      name: "Batavia",
+      nom: "Batavia",
+      image: "assets/batavia.jpg",
+    },
+    {
+      code: "8",
+      name: "Kiwi vert",
+      nom: "Kiwi vert",
+      image: "assets/kiwi_vrac.jpg",
+    },
+    {
+      code: "9",
+      name: "Radis botte",
+      nom: "Radis botte",
+      image: "assets/radis_vrac.jpg",
+    },
+    {
+      code: "10",
+      name: "Raisin blanc",
+      nom: "Raisin blanc",
+      image: "assets/raisin_blanc.jpg",
+    },
+    {
+      code: "11",
+      name: "Aubergine",
+      nom: "Aubergine",
+      image: "assets/aubergine_vrac.jpg",
+    },
+    {
+      code: "12",
+      name: "Chou-fleur",
+      nom: "Chou-fleur",
+      image: "assets/chou_fleur.jpg",
+    },
+    {
+      code: "13",
+      name: "Poire verte",
+      nom: "Poire verte",
+      image: "assets/poire_vrac.jpg",
+    },
+    {
+      code: "14",
+      name: "Mangue",
+      nom: "Mangue",
+      image: "assets/mangue_vrac.jpg",
+    },
+    {
+      code: "15",
+      name: "Pomelo",
+      nom: "Pomelo",
+      image: "assets/pamplemousse_vrac.jpg",
+    },
+    {
+      code: "16",
+      name: "Ananas",
+      nom: "Ananas",
+      image: "assets/ananas.jpg",
+    },
+    {
+      code: "17",
+      name: "Nectarine",
+      nom: "Nectarine",
+      image: "assets/nectarine.jpg",
+    },
+    {
+      code: "18",
+      name: "Pomme bicolore",
+      nom: "Pomme bicolore",
+      image: "assets/pomme_gala.jpg",
+    },
+    {
+      code: "19",
+      name: "Pomme Golden",
+      nom: "Pomme Golden",
+      image: "assets/pomme_golden.jpg",
+    },
+    {
+      code: "20",
+      name: "Patate douce",
+      nom: "Patate douce",
+      image: "assets/patate_douce_vrac.jpg",
+    },
+
     // Boulangerie
-    { code: "50", name: "Pain Chocolat",       nom: "Pain Chocolat",       image: "pain_chocolat_pur_beurre.jpg" },
-    { code: "51", name: "Beignet Chocolat",    nom: "Beignet Chocolat",    image: "beignet_chocolat_noisette.jpg" },
-    { code: "52", name: "Croissant",           nom: "Croissant",           image: "croissant.jpg" },
-    { code: "53", name: "Ciabatta",            nom: "Ciabatta",            image: "ciabatta.jpg" },
-    { code: "54", name: "Pain aux Noix",       nom: "Pain aux Noix",       image: "pain_noix.jpg" },
-    { code: "55", name: "Pain Céréales",       nom: "Pain Céréales",       image: "pain_multicereales.jpg" },
-    { code: "56", name: "Pain Complet",        nom: "Pain Complet",        image: "petit_pain_complet.jpg" },
-    { code: "57", name: "Suisse Chocolat",     nom: "Suisse Chocolat",     image: "suisse_chocolat.jpg" },
-    { code: "58", name: "Barchetta 4 Fromages",nom: "Barchetta 4 Fromages",image: "barchetta_4_fromages.jpg" },
-    { code: "59", name: "Pain aux Raisins",    nom: "Pain aux Raisins",    image: "pain_aux_raisins.jpg" }
+    {
+      code: "50",
+      name: "Chocolatine",
+      nom: "Chocolatine",
+      image: "assets/pain_chocolat_pur_beurre.jpg",
+    },
+    {
+      code: "51",
+      name: "Beignet chocolat",
+      nom: "Beignet chocolat",
+      image: "assets/beignet_chocolat_noisette.jpg",
+    },
+    {
+      code: "52",
+      name: "Croissant",
+      nom: "Croissant",
+      image: "assets/croissant.jpg",
+    },
+    {
+      code: "53",
+      name: "Ciabatta",
+      nom: "Ciabatta",
+      image: "assets/ciabatta.jpg",
+    },
+    {
+      code: "54",
+      name: "Pain aux noix",
+      nom: "Pain aux noix",
+      image: "assets/pain_noix.jpg",
+    },
+    {
+      code: "55",
+      name: "Pain céréales",
+      nom: "Pain céréales",
+      image: "assets/pain_multicereales.jpg",
+    },
+    {
+      code: "56",
+      name: "Pain complet",
+      nom: "Pain complet",
+      image: "assets/petit_pain_complet.jpg",
+    },
+    {
+      code: "57",
+      name: "Suisse chocolat",
+      nom: "Suisse chocolat",
+      image: "assets/suisse_chocolat.jpg",
+    },
+    {
+      code: "58",
+      name: "Barchetta",
+      nom: "Barchetta",
+      image: "assets/barchetta_4_fromages.jpg",
+    },
+    {
+      code: "59",
+      name: "Pain aux raisins",
+      nom: "Pain aux raisins",
+      image: "assets/pain_aux_raisins.jpg",
+    },
   ];
 
-  const questions = [
-    // Exemples — tu peux ajouter/réviser à ta convenance
-    { id: "q1", productCode: "1",  question: "Quel est le code pour « Banane » ?", 
-      choices: ["1","2","10","50"], answerIndex: 0, explanation: "Banane = code 1" },
-    { id: "q2", productCode: "2",  question: "Quel code correspond à « Tomate » ?", 
-      choices: ["3","2","19","52"], answerIndex: 1, explanation: "Tomate = code 2" },
-    { id: "q3", productCode: "52", question: "Quel est le nom du produit code 52 ?", 
-      choices: ["Croissant","Pain Chocolat","Ciabatta","Pain Complet"], answerIndex: 0, explanation: "52 = Croissant" }
-    // … ajoute autant que nécessaire
-  ];
-
-  return { products, questions };
+  // On renvoie un objet avec la liste des produits
+  return {
+    products,
+  };
 })();
