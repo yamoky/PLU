994d982e13beb2339900c62c8c1c9d662431a827
yamoky <cyrille.closier@gmail.com>
2025-12-11 18:51:57 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 // data.js
-// Base de données des produits PLU + images
+// Base de données des produits PLU + images + catégories
 
 window.PLU_DATA = (function () {
   const products = [
@@ -9,185 +9,71 @@ window.PLU_DATA = (function () {
       name: "Banane",
       nom: "Banane",
       image: "assets/banane.jpg",
+      category: "fruits_legumes",
     },
     {
       code: "2",
       name: "Grappe de tomate",
       nom: "Grappe de tomate",
       image: "assets/tomate_grappe.jpg",
+      category: "fruits_legumes",
     },
     {
       code: "3",
       name: "Concombre",
       nom: "Concombre",
       image: "assets/concombre.jpg",
+      category: "fruits_legumes",
     },
     {
       code: "4",
       name: "Courgette",
       nom: "Courgette",
       image: "assets/courgette_vrac.jpg",
+      category: "fruits_legumes",
     },
     {
       code: "5",
       name: "Melon",
       nom: "Melon",
       image: "assets/melon.jpg",
+      category: "fruits_legumes",
     },
     {
       code: "6",
       name: "Avocat",
       nom: "Avocat",
       image: "assets/avocat_vrac.jpg",
+      category: "fruits_legumes",
     },
     {
       code: "7",
       name: "Batavia",
       nom: "Batavia",
       image: "assets/batavia.jpg",
+      category: "fruits_legumes",
     },
     {
       code: "8",
       name: "Kiwi vert",
       nom: "Kiwi vert",
       image: "assets/kiwi_vrac.jpg",
+      category: "fruits_legumes",
     },
     {
       code: "9",
       name: "Radis botte",
       nom: "Radis botte",
       image: "assets/radis_vrac.jpg",
+      category: "fruits_legumes",
     },
     {
       code: "10",
       name: "Raisin blanc",
       nom: "Raisin blanc",
       image: "assets/raisin_blanc.jpg",
+      category: "fruits_legumes",
     },
     {
       code: "11",
       name: "Aubergine",
-      nom: "Aubergine",
-      image: "assets/aubergine_vrac.jpg",
-    },
-    {
-      code: "12",
-      name: "Chou-fleur",
-      nom: "Chou-fleur",
-      image: "assets/chou_fleur.jpg",
-    },
-    {
-      code: "13",
-      name: "Poire verte",
-      nom: "Poire verte",
-      image: "assets/poire_vrac.jpg",
-    },
-    {
-      code: "14",
-      name: "Mangue",
-      nom: "Mangue",
-      image: "assets/mangue_vrac.jpg",
-    },
-    {
-      code: "15",
-      name: "Pomelo",
-      nom: "Pomelo",
-      image: "assets/pamplemousse_vrac.jpg",
-    },
-    {
-      code: "16",
-      name: "Ananas",
-      nom: "Ananas",
-      image: "assets/ananas.jpg",
-    },
-    {
-      code: "17",
-      name: "Nectarine",
-      nom: "Nectarine",
-      image: "assets/nectarine.jpg",
-    },
-    {
-      code: "18",
-      name: "Pomme bicolore",
-      nom: "Pomme bicolore",
-      image: "assets/pomme_bicolore_vrac.jpg",
-    },
-    {
-      code: "19",
-      name: "Pomme Golden",
-      nom: "Pomme Golden",
-      image: "assets/pomme_golden.jpg",
-    },
-    {
-      code: "20",
-      name: "Patate douce",
-      nom: "Patate douce",
-      image: "assets/patate_douce_vrac.jpg",
-    },
-
-    // Boulangerie
-    {
-      code: "50",
-      name: "Chocolatine",
-      nom: "Chocolatine",
-      image: "assets/pain_chocolat_pur_beurre.jpg",
-    },
-    {
-      code: "51",
-      name: "Beignet chocolat",
-      nom: "Beignet chocolat",
-      image: "assets/beignet_chocolat_noisette.jpg",
-    },
-    {
-      code: "52",
-      name: "Croissant",
-      nom: "Croissant",
-      image: "assets/croissant.jpg",
-    },
-    {
-      code: "53",
-      name: "Ciabatta",
-      nom: "Ciabatta",
-      image: "assets/ciabatta.jpg",
-    },
-    {
-      code: "54",
-      name: "Pain aux noix",
-      nom: "Pain aux noix",
-      image: "assets/pain_noix.jpg",
-    },
-    {
-      code: "55",
-      name: "Pain céréales",
-      nom: "Pain céréales",
-      image: "assets/pain_multicereales.jpg",
-    },
-    {
-      code: "56",
-      name: "Pain complet",
-      nom: "Pain complet",
-      image: "assets/petit_pain_complet.jpg",
-    },
-    {
-      code: "57",
-      name: "Suisse chocolat",
-      nom: "Suisse chocolat",
-      image: "assets/suisse_chocolat.jpg",
-    },
-    {
-      code: "58",
-      name: "Barchetta",
-      nom: "Barchetta",
-      image: "assets/barchetta_4_fromages.jpg",
-    },
-    {
-      code: "59",
-      name: "Pain aux raisins",
-      nom: "Pain aux raisins",
-      image: "assets/pain_aux_raisins.jpg",
-    },
-  ];
-
-  return { products };
-})();
-
